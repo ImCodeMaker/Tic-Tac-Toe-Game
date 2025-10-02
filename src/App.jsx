c626645363bead46b69ec0bfed09aa0512c20082
Teudy Encarnacion <teudyencarnacion@macbookpro.lan>
2025-10-01 20:46:39 -0400

    added this thing

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
     currentSquares[userInput] = isXNext ? "X" : "O";
     setSquare(currentSquares);
     setIsXNext(!isXNext);
+    //XD
 
     let winnerResult = checkWinner(currentSquares);
 
